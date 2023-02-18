45f13050282c909c6180b4cd828554ee2a8b9afe
IamBaghel-0 <ysbg5003639@gmail.com>
2023-02-18 17:06:15 +0530

    Newton School Assignment

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  // Create a state variable "inputValue" with an initial value of an empty string
+  const [inputValue, setInputValue] = useState("");  
+  // Create a reference "count" using the useRef hook and initialize it to zero
+  const count = useRef(0);  
+
+  useEffect(() => {  
+    // Create an effect that will run after every render of the component
+    // Increment the count by 1 using the reference "count" created above
+    count.current = count.current + 1;  
+  });
+
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        // Update the state "inputValue" with the current value of the input field
+        onChange={(e) => setInputValue(e.target.value)}  
+      />
+      {/* Display the current value of the "count" reference in the heading */}
+      <h1>Render Count: {count.current}</h1>  
+    </>
+  );
+}
+
+// Create a root element for the React application
+const root = ReactDOM.createRoot(document.getElementById('root'));  
+// Render the App component inside the root element created above
+root.render(<App />);  
